70022df0b89b3d1e688ad221d32b199e2c350f4c
taeiim <ti535666@gmail.com>
2018-08-29 15:39:26 +0900

    Update QueryTvSales.java

--- SimpleDataQuery/src/main/java/QueryTvSales.java
+++ SimpleDataQuery/src/main/java/QueryTvSales.java
@@ -18,6 +18,8 @@ public class QueryTvSales {
 		sales.add(Pair.create("TV", 3000));
 		sales.add(Pair.create("Phone", 800));
 		
+		// Observable이 아닌 Maybe로 한 이유 
+		// "TV" 대신 "tv"로 해보면(key값 다르게) 결과값이 없음.  -> 만약 Observable로 했으면 Error 났을 것! 
 		Maybe<Integer> tvSales = io.reactivex.Observable.fromIterable(sales)
 				.filter(sale -> "TV".equals(sale.getKey()))
 				.map(sale -> sale.getValue())
